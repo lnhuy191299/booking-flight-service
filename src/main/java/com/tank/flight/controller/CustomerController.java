54b65002bd804ecfbfcdd99bddbb02986bc8fd87
Ngoc Huy <lnhuy1912@gmail.com>
2022-04-20 22:55:32 +0700

    first commit

---
+++ src/main/java/com/tank/flight/controller/CustomerController.java
@@ -0,0 +1,14 @@
+package com.tank.flight.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+  @GetMapping("")
+  public ResponseEntity<String> greeting() {
+    return ResponseEntity.ok("Customer service work!");
+  }
+}
